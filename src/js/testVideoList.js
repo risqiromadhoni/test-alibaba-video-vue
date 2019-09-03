93d23d0fb2afe033b4eee51551179b970eb2ae0c
risqi romadhoni <risqsir57@gmail.com>
2019-09-03 16:24:04 +0700

    Initial Commit

---
+++ src/js/testVideoList.js
@@ -0,0 +1,22 @@
+export const videoList = [{
+  VideoId: 0,
+  source: 'http://cdnvideo.babastudio.com/sv/1cbacad8-16c431912e6/1cbacad8-16c431912e6.mp4',
+  CoverURL: 'http://vod-download.cn-shanghai.aliyuncs.com/video-list-img/cctv5plus.png',
+  Title: 'Bekerja Dengan GitHub',
+  Duration: '00:00',
+  Description: 'Test Video Server Jakarta'
+}, {
+  VideoId: 1,
+  source: 'http://cdnvideo.babastudio.com/sv/55ef562c-16c4318ed72/55ef562c-16c4318ed72.mp4',
+  CoverURL: 'http://cdnvideo.babastudio.com/2b57c1f4430a4bff9b50278e9afb5ebf/snapshots/34ba40b216c64920b88c4679c5e5a6dc-00004.jpg',
+  Title: 'Menggunakan Percabaangan untuk Mencegah Konflik',
+  Duration: '00:00',
+  Description: 'Test Video Server Jakarta'
+}, {
+  VideoId: 2,
+  source: 'http://cdnvideo.babastudio.com/sv/42de9eab-16c4318ed65/42de9eab-16c4318ed65.mp4',
+  CoverURL: 'http://cdnvideo.babastudio.com/15701c2738ab4abfb183a688d1d6b870/snapshots/ddddeb6d26a645ab8335313c97ad0407-00005.jpg',
+  Title: 'LAVILLENIE CLAIMS POLE VAULT RECORD IN LONDON 2012',
+  Duration: '00:00',
+  Description: 'Perintah Untuk Membatalkan Suatu Revisi atau Perubahan Dalam Git'
+}]
